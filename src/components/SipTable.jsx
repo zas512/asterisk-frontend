25a582d331e2adf734cbfbe4c3edd0be7c76958f
zas512 <zainalis.914@gmail.com>
2023-06-27 10:10:31 +0500

    design modify

--- src/components/SipTable.jsx
+++ src/components/SipTable.jsx
@@ -56,49 +56,49 @@ function SipTable() {
         <table className="w-full h-auto text-sm text-left text-gray-900 shadow-lg">
           <thead className="text-xs text-white bg-gray-800">
             <tr>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 NAME
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 DISALLOW
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 HOST
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 ALLOW
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 TYPE
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 SECRET
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 DTMF MODE
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 QUALIFY
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 CAN RE-INVITE
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 INSECURE
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 NAT
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 DIRECT MEDIA
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 DIRECT RTP SETUP
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 CONTEXT
               </th>
-              <th scope="col" className="px-4 py-3">
+              <th scope="col" className="px-2 py-3">
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg w-20 text-xs px-2 py-2.5"
@@ -132,23 +132,23 @@ function SipTable() {
                   key={SIP_ID}
                   className="bg-white border-black hover:bg-gray-100"
                 >
-                  <td className="px-4 py-4 font-medium text-gray-900">
+                  <td className="px-2 py-4 font-medium text-gray-900">
                     {SIP_ID}
                   </td>
-                  <td className="px-4 py-4">{DISALLOW}</td>
-                  <td className="px-4 py-4">{HOST}</td>
-                  <td className="px-4 py-4">{ALLOW}</td>
-                  <td className="px-4 py-4">{TYPE}</td>
-                  <td className="px-4 py-4">{SECRET}</td>
-                  <td className="px-4 py-4">{DTMFMODE}</td>
-                  <td className="px-4 py-4">{QUALIFY}</td>
-                  <td className="px-4 py-4">{CANREINVITE}</td>
-                  <td className="px-4 py-4">{INSECURE}</td>
-                  <td className="px-4 py-4">{NAT}</td>
-                  <td className="px-4 py-4">{DIRECTMEDIA}</td>
-                  <td className="px-4 py-4">{DIRECTRTPSETUP}</td>
-                  <td className="px-4 py-4">{CONTEXT}</td>
-                  <td className="flex px-4 py-4">
+                  <td className="px-2 py-4">{DISALLOW}</td>
+                  <td className="px-2 py-4">{HOST}</td>
+                  <td className="px-2 py-4">{ALLOW}</td>
+                  <td className="px-2 py-4">{TYPE}</td>
+                  <td className="px-2 py-4">{SECRET}</td>
+                  <td className="px-2 py-4">{DTMFMODE}</td>
+                  <td className="px-2 py-4">{QUALIFY}</td>
+                  <td className="px-2 py-4">{CANREINVITE}</td>
+                  <td className="px-2 py-4">{INSECURE}</td>
+                  <td className="px-2 py-4">{NAT}</td>
+                  <td className="px-2 py-4">{DIRECTMEDIA}</td>
+                  <td className="px-2 py-4">{DIRECTRTPSETUP}</td>
+                  <td className="px-2 py-4">{CONTEXT}</td>
+                  <td className="flex px-2 py-4">
                     <button
                       className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 w-14 font-medium rounded-lg text-xs px-2 py-2.5 mr-2"
                       onClick={() => toggleEditPopup(SIP_ID)}
